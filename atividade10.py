a7db5ecece1f21009449fcad61eade44ee09895e
Dadson097 <antoniodadsonoliveira@gmail.com>
2025-09-25 14:26:42 -0300

    Add files via upload

---
+++ atividade10.py
@@ -0,0 +1,8 @@
+# Atividade 10 – Conversão de Unidades
+# Peça ao usuário um valor em metros.
+# Converta e mostre o resultado em centímetros e milímetros.
+metros = int(input("Digite um valor em metros: "))
+centimetros = metros * 100
+melimetros = metros * 1000
+print(centimetros)
+print(melimetros)
